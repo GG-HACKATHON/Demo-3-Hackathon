99d8db834f6d1e641490dc72b46fc1bcae9797cb
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2017-05-21 10:10:16 +0700

    - map manage

--- Technical/Demo 3/Assets/Scripts/Core/Map/MapItem.cs
+++ Technical/Demo 3/Assets/Scripts/Core/Map/MapItem.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum MAPTYPE
+{
 
+}
 public class MapItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public SpriteRenderer sprte;
+    public int col;
+    public int row;
+    public MAPTYPE type;
+    public void Parse(int col, int row, MAPTYPE type)
+    {
+        this.col = col;
+        this.row = row;
+        this.type = type;
+        //tao cac position o day
+    }
+    public void OnCollision()
+    { }
 }
--- Technical/Demo 3/Assets/Scripts/Core/Map/MapManager.cs
+++ Technical/Demo 3/Assets/Scripts/Core/Map/MapManager.cs
@@ -2,15 +2,82 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapManager : MonoBehaviour {
+public class MapManager : MonoSingleton<MapManager> {
+    public const int MAP_WIDTH = 9;
+    public const int MAP_HIGHT = 9;
+    [System.Serializable]
+    public class MapModel
+    {
+        public MAPTYPE type;
+        public MapItem item;
+    }
+    [System.Serializable]
+    public class Map
+    {
+        public int col;
+        public int row;
+        public MapItem item;
+        public Map()
+        {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        }
+        public Map(int col, int row, MapItem item)
+        {
+            this.col = col;
+            this.row = row;
+            this.item = item;
+        }
+    }
+    public List<MapModel> models;
+    public Dictionary<int,Dictionary<int,Map>> maps;//row-ColMap
+    public Map playerCurrent;//vi tri hien tai cua player tren map
+    private Dictionary<MAPTYPE, MapModel> dicModel;
+
+    void Awake()
+    {
+        this.dicModel = new Dictionary<MAPTYPE, MapModel>();
+        this.maps = new Dictionary<int, Dictionary<int, Map>>();
+    }
+    //khoi tao map ban dau
+    public void CreateMap()
+    {
+        for(int row = 0; row < MAP_HIGHT; row ++)
+        {
+            Dictionary<int, Map> mapCol = new Dictionary<int, Map>();
+            for(int col = 0; col <MAP_WIDTH; col++)
+            {
+                int index = Random.Range(0, 4);//random map type
+                Map map = new Map(row, col, null);
+                if(!mapCol.ContainsKey(col))
+                {
+                    mapCol.Add(col, map);
+                }
+            }
+            if(!this.maps.ContainsKey(row))
+            {
+                this.maps.Add(row, mapCol);
+            }
+        }
+    }
+    public Map GetMapItem(int row, int col)
+    {
+        if(this.maps.ContainsKey(row))
+        {
+            Dictionary<int, Map> mapRow = this.maps[row];
+            if(mapRow.ContainsKey(col))
+            {
+                return this.maps[row][col];
+            }
+        }
+        return null;
+    }
+    public Map PlayerPosition()
+    {
+        Player player = PlayerController.Instance.mainPlayer;
+        BaseBody head = player.head;
+        //kiem tra vi tri cua no tren ban do
+        //tra ve ket qua la row, col, positon
+        return null;
+    }
+
 }
