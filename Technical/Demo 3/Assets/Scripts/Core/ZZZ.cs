cf42716fbe680b0e43a5277831f48efcc34c6335
Khang Dinh Hoang <taigama@users.noreply.github.com>
2017-05-21 11:12:08 +0700

    swipe + build demo

---
+++ Technical/Demo 3/Assets/Scripts/Core/ZZZ.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZZZ : MonoBehaviour
+{
+
+	public void Up()
+    {
+        transform.position += Vector3.up * 0.5f;
+    }
+
+    public void Down()
+    {
+        transform.position += Vector3.down * 0.5f;
+    }
+
+    public void Left()
+    {
+        transform.position += Vector3.left * 0.5f;
+    }
+
+    public void Right()
+    {
+        transform.position += Vector3.right * 0.5f;
+    }
+}
